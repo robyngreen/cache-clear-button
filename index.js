1cb34229b26087745161fdc1e0c30a4617d25603
Robyn Green <robyn.green@mediacurrent.com>
2017-08-29 22:55:56 -0500

    Added button support

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 'use strict';
 
+// Load required libraries for interacting with the pi.
 const Raspi = require('raspi-io');
-const five = require('johnny-five');
-const board = new five.Board({
+const shortCircuit = require('johnny-five');
+
+// Delcare a new object to reference our breadboard.
+const board = new shortCircuit.Board({
   io: new Raspi()
-})
+});
+
+board.on('ready', () => {
+  let button = shortCircuit.Button('18');
+
+  board.repl.inject({
+    button: button
+  });
+
+  button.on('down', function() {
+    console.log('down');
+  });
+
+  button.on('hold', function() {
+    console.log('hold');
+  });
+
+  button.on('up', function() {
+    console.log('up');
+  });
+
+});
